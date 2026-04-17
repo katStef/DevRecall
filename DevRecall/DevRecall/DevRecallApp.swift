38486112bfc84287af15feaae2a035866abc4564
katherine.stefaniak <katherine.stefaniak@accenture.com>
2026-04-17 12:27:09 +0200

    Initial Xcode project setup

---
+++ DevRecall/DevRecall/DevRecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevRecallApp.swift
+//  DevRecall
+//
+//  Created by katherine.stefaniak on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct DevRecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DevRecall/DevRecallTests/DevRecallTests.swift
@@ -0,0 +1,17 @@
+//
+//  DevRecallTests.swift
+//  DevRecallTests
+//
+//  Created by katherine.stefaniak on 17/04/26.
+//
+
+import Testing
+@testable import DevRecall
+
+struct DevRecallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
